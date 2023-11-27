7ac98e14c69a6983e540a69402f1e943339b55ee
Beau McCartney <mccartney.beausl@gmail.com>
2023-11-27 13:37:47 -0700

    update

--- stow_folder/.config/fish/config.fish
+++ stow_folder/.config/fish/config.fish
@@ -4,17 +4,7 @@ bass source ~/.zshenv
 
 set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ;
 
-set --export BUN_INSTALL "$HOME/.bun"
-
-set --export PYENV_ROOT $HOME/.pyenv
-
-set --export CARGO_TARGET_DIR $HOME/.cache/cargo-target
-
-set --export RIPGREP_CONFIG_PATH $DOTFILES/ripgrepconfig
-
-fish_add_path $HOME/.local/bin $BUN_INSTALL/bin $PYENV_ROOT/bin $HOME/.cabal/bin /Users/beaum/.ghcup/bin
-
-eval $(/opt/homebrew/bin/brew shellenv)
+eval (/opt/homebrew/bin/brew shellenv)
 
 if not status is-interactive
     exit
@@ -29,16 +19,6 @@ set fish_cursor_visual      block
 
 set fish_color_command green --bold
 
-# pnpm
-set -gx PNPM_HOME "/Users/beaum/Library/pnpm"
-if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
-end
-# pnpm end
-
-abbr npm pnpm
-abbr npx pnpx
-
 alias e $VISUAL
 
 alias mkdir 'mkdir -p'
@@ -49,11 +29,7 @@ alias lmr 'll  --sort=time --reverse'
 
 alias zigup 'zigup --install-dir ~/thirdparty/zig'
 
-alias dungeon '~/dungeon-note/dungeon.py'
-
 abbr lg 'lazygit'
-
-pyenv init - | source
 
 zoxide init fish | source
 
--- stow_folder/.zshenv
+++ stow_folder/.zshenv
@@ -1,6 +1,17 @@
+eval "$(/opt/homebrew/bin/brew shellenv)"
+eval "$(/usr/libexec/path_helper)"
+
 export DOTFILES=$HOME/macdots
 
 export EDITOR="nvim"
 export VISUAL="nvim"
 
 export MANPAGER="sh -c 'col -bx | bat -l man -p'"
+
+export BUN_INSTALL="$HOME/.bun"
+export CARGO_TARGET_DIR="$HOME/.cache/cargo-target"
+export RIPGREP_CONFIG_PATH="$DOTFILES/ripgrepconfig"
+
+export PNPM_HOME="$HOME/Library/pnpm"
+
+export PATH="$HOME/.local/bin:$(brew --prefix llvm)/bin:$BUN_INSTALL/bin:$HOME/.cabal/bin:$HOME/.ghcup/bin:$PNPM_HOME:$PATH"
