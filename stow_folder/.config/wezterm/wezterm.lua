7d3ead0ad16bb4db00d6c62f1b6dec414f5ee8c6
Beau McCartney <mccartney.beausl@gmail.com>
2023-12-12 23:43:38 -0700

    wezterm colors + other stuff
    the animation stuff is absolutely cursed beyond belief

--- stow_folder/.config/wezterm/wezterm.lua
+++ stow_folder/.config/wezterm/wezterm.lua
@@ -17,4 +17,52 @@ config.window_padding = {
     bottom = 0,
 }
 
+config.default_cursor_style = 'SteadyBlock'
+config.animation_fps = 1
+config.cursor_blink_ease_in = 'Constant'
+config.cursor_blink_ease_out = 'Constant'
+
+local white       = '#f7f1ff'
+local dull_black  = '#363537'
+local light_black = '#69676c'
+local pureblack   = '#000000'
+local red         = '#fc618d'
+local green       = '#7bd88f'
+local yellow      = '#fce566'
+local blue        = '#fd9353'
+local magenta     = '#948ae3'
+local cyan        = '#5ad4e6'
+
+config.colors = {
+    background   = pureblack,
+    foreground   = white,
+
+    selection_fg = dull_black,
+    selection_bg = white,
+
+    cursor_fg    = pureblack,
+    cursor_bg    = white,
+
+    ansi = {
+        dull_black,
+        red,
+        green,
+        yellow,
+        blue,
+        magenta,
+        cyan,
+        white,
+    },
+    brights = {
+        light_black,
+        red,
+        green,
+        yellow,
+        blue,
+        magenta,
+        cyan,
+        white,
+    },
+}
+
 return config
