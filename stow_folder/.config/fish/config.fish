02d0d8a2b5f8718c6d96e1a7eebd0dbdb009bf0a
Beau McCartney <mccartney.beausl@gmail.com>
2024-05-24 22:04:18 -0600

    ls alias doesn't show dotfiles - use la

--- stow_folder/.config/fish/config.fish
+++ stow_folder/.config/fish/config.fish
@@ -23,7 +23,8 @@ alias fd "fd --hidden --no-ignore-vcs --follow"
 
 alias mkdir 'mkdir -p'
 
-alias ls 'eza --all --oneline'
+alias ls 'eza --oneline'
+alias la 'eza --all --oneline'
 alias ll 'eza -la --git'
 alias lmr 'll --sort=time --reverse'
 
