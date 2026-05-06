45a3765665ee66a9789f5980454af4f44eed3eb7
Beau McCartney <mccartney.beausl@gmail.com>
2026-05-06 14:40:39 -0600

    set up store
    store the dotfile manager tool here: https://github.com/cushycush/store

---
+++ fish/config.fish
@@ -0,0 +1,67 @@
+set fish_greeting
+
+set --export EDITOR "nvim"
+set --export VISUAL "nvim"
+
+set --export MANPAGER "sh -c 'col -bx | bat --language man -p'"
+
+set --export RIPGREP_CONFIG_PATH "$HOME/macdots/ripgrepconfig"
+
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PNPM_HOME "$HOME/Library/pnpm"
+
+# HACK: make things (install scripts, etc.) that detect shell with $SHELL work
+set --export SHELL fish
+
+eval (/opt/homebrew/bin/brew shellenv)
+
+# ensure these are prepended on $PATH
+fish_add_path   --move                                \
+                "$HOME/.local/bin"                    \
+                "$PNPM_HOME"                          \
+                "$BUN_INSTALL/bin"                    \
+                "$HOME/.elan/bin"
+
+alias e $VISUAL
+
+alias fd "fd --hidden --no-ignore-vcs --follow"
+
+alias mkdir 'mkdir -pv'
+
+alias ls 'eza --oneline'
+alias la 'eza --all --oneline'
+alias ll 'eza --long --all --git'
+alias lmr 'll --sort=time --reverse'
+
+abbr g 'git'
+abbr lg 'lazygit'
+
+function dequarantine --wraps rm --description 'dequarantine passsed files/dirs'
+    sudo xattr -d -r com.apple.quarantine $argv
+end
+
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if read -z cwd < "$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
+
+if not status is-interactive
+    exit
+end
+
+fish_vi_key_bindings
+fish_vi_cursor
+set fish_cursor_insert line
+
+set fish_color_command green --bold
+
+alias gvm 'g' # protect 'g' alias for git
+source "$HOME/.g/env.fish"
+
+zoxide init fish | source
+
+starship init fish | source
---
+++ pi/AGENTS.md
@@ -0,0 +1,1 @@
+Ignore and do NOT read all files ignored by git via the global git ignore or .git/info/exclude
